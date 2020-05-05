96ddf061f352114a25fc1eee80aa2f8f64aeeb76
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-05-05 18:11:42 +0200

    Changed round function to be more consistent

--- benchmark/util.ts
+++ benchmark/util.ts
@@ -1,4 +1,3 @@
 export function round(num: number, decimalPlaces: number = 0) {
-    let mult = Math.pow(10, decimalPlaces);
-    return math.floor(num * mult + 0.5) / mult;
+    return tonumber(string.format(`%.${decimalPlaces}f`, num))
 }
