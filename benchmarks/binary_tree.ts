4053fb094c511e5d5f3b951c1ab6cf65af944d0f
Lorenz Junglas <lolleko@users.noreply.github.com>
2020-05-05 12:52:14 +0200

    Slightly reduce consumption

--- benchmarks/binary_tree.ts
+++ benchmarks/binary_tree.ts
@@ -7,7 +7,7 @@
 */
 
 export function binaryTreeBenchmark() {
-    const maxDepth = 18;
+    const maxDepth = 17;
 
     const stretchDepth = maxDepth + 1;
     const check = itemCheck(bottomUpTree(stretchDepth));
